45508bdbd50eda59d705b9998be0c61c8d49057e
keeplooking6 <1422910966@qq.com>
2024-06-24 11:32:07 +0800

    upload hw01

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -13,11 +13,12 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = lambda a,b: a - b
     else:
-        f = _____
+        f = lambda a,b: a + b
     return f(a, b)
 
+# !!!
 def a_plus_abs_b_syntax_check():
     """Check that you didn't change the return statement of a_plus_abs_b.
 
@@ -28,6 +29,31 @@ def a_plus_abs_b_syntax_check():
     """
     # You don't need to edit this function. It's just here to check your work.
 
+# 我这种做法虽然正确，但是不符合作业预期
+# def two_of_three(i, j, k):
+#     """Return m*m + n*n, where m and n are the two smallest members of the
+#     positive numbers i, j, and k.
+#
+#     >>> two_of_three(1, 2, 3)
+#     5
+#     >>> two_of_three(5, 3, 1)
+#     10
+#     >>> two_of_three(10, 2, 8)
+#     68
+#     >>> two_of_three(5, 5, 5)
+#     50
+#     """
+#     min_num = min(i, j, k)
+#     if min_num == i:
+#         min_num_2 = min(j, k)
+#     elif min_num == j:
+#         min_num_2 = min(i, k)
+#     else:
+#         min_num_2 = min(i, j)
+#     return min_num**2 + min_num_2**2
+
+
+# !!!
 
 def two_of_three(i, j, k):
     """Return m*m + n*n, where m and n are the two smallest members of the
@@ -42,7 +68,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return sum(sorted([i**2,j**2,k**2])[:2])
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,7 +92,34 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    # 1
+    # 2:1
+    # 3:1
+    # 4:2
+    # 5:1
+    # 6:2,3
+    # 7:1
+    # 8:1,2,4
+    # 9:1,3
+    # 10:1,2,5
+    # 11:1
+    # 12:1,2,3,46
+    # 13:1
+    # if n % 2
+#     质数怎么通过方法判断
+# 通过去取余比自己小的数，如果有==0的情况，说明不是质数，则将该因数放入列表，后续从列表中拿出最大的数
+    if n == 1:
+        return None
+    if n == 2:
+        return 1
+    num = 2
+    factor_list = [1,]
+    while num < n:
+        if n % num == 0:
+            factor_list.append(num)
+        num += 1
 
+    return max(factor_list)
 
 def hailstone(n):
     """Print the hailstone sequence starting at n and return its
@@ -88,4 +141,16 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
+    length = 0
+    if n == 1:
+        print(n)
+        return 1
+    while n != 1:
+        print(n)
+        length += 1
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    print(n)
+    return length + 1
